2de2e010f56991f5c9d272f13fae33bedd86eb73
Jose <jcabrerap@cifpfbmoll.eu>
2021-02-03 17:07:18 +0100

    ajustar

--- mongo_init/db.js
+++ mongo_init/db.js
@@ -1,73 +1,64 @@
 // db.js
 db.restaurants.insert({
-    name: "Bar Tolo"
-    rate: 7
-    locality: "Palma"
-    price index: 5.5
+    name: "Bar Tolo",
+    rate: 7,
+    locality: "Palma",
+    price_index: 5.5,
     phone: 684125874
 
 
   })
   db.restaurants.insert({
-    name: "Bar Fuentes"
-    rate: 4
-    locality: "Inca"
-    price index: 3.33
+    name: "Bar Fuentes",
+    rate: 4,
+    locality: "Inca",
+    price_index: 3.33,
     phone: 687123497
     
 
   })
   db.restaurants.insert({
-    name: "Bar Bocados"
-    rate: 10
-    locality: "Sa indioteria"
-    price index: 6.2
+    name: "Bar Bocados",
+    rate: 10,
+    locality: "Sa indioteria",
+    price_index: 6.2,
     phone: 658124879
     
 
   })
   db.restaurants.insert({
-    name: "Bar Frontera"
-    rate: 6
-    locality: "Palma"
-    price index: 10.95
+    name: "Bar Frontera",
+    rate: 6,
+    locality: "Palma",
+    price_index: 10.95,
     phone: 658124982
     
 
   })
   db.restaurants.insert({
-    name: "Bar Toledo"
-    rate: 5
-    locality: "Manacor"
-    price index: 8.94
+    name: "Bar Toledo",
+    rate: 5,
+    locality: "Manacor",
+    price_index: 8.94,
     phone: 658412638
     
 
   })
   db.restaurants.insert({
-    name: "Bar Ruben"
-    rate: 6.5
-    locality: "Consell"
-    price index: 5
+    name: "Bar Ruben",
+    rate: 6.5,
+    locality: "Consell",
+    price_index: 5,
     phone: 648712985
     
 
   })
   db.restaurants.insert({
-    name: "Bar Mizi"
-    rate: 7
-    locality: "Palma"
-    price index: 2.33
+    name: "Bar Mizi",
+    rate: 7,
+    locality: "Palma",
+    price_index: 2.33,
     phone: 648721698
-    
-
-  })
-  db.restaurants.insert({
-    name: "Bar Tolo"
-    rate: 7
-    locality:
-    price index:
-    phone:
     
 
   })
